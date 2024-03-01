c6253bc9bfc8b2749492912ae968e73165a5597e
KatSchreiner <kat.schreiner@yandex.ru>
2024-03-02 00:16:19 +0300

    Добавлен LaunchScreen

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Екатерина Шрайнер on 01.03.2024.
+//
+
+import UIKit
+
+extension UIColor {
+     static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+}
